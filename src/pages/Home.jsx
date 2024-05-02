7cb6a484fbdd4aeaee4d7180c2334e55dfedc2f1
jishasree27 <162873324+jishasree27@users.noreply.github.com>
2024-05-02 23:40:39 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Agalya V</strong>
+                <strong className="main-name"> Jisha Sree A</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
